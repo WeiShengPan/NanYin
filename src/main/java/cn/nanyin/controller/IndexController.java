7d9ff8152c76d361bc519a3cc89c3edab12dd169
WeiShengPan <pws522@qq.com>
2015-07-08 14:59:28 +0800

    1

--- src/main/java/cn/nanyin/controller/IndexController.java
+++ src/main/java/cn/nanyin/controller/IndexController.java
@@ -1,5 +1,6 @@
 package cn.nanyin.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/index.jsp", method = RequestMethod.GET)
-    public ModelAndView index() {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index(int n) {
+
         return new ModelAndView("index");
     }
+
+
 }
