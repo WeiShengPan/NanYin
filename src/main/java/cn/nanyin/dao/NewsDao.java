6e71869aa5ee769a971ab1a8244cce84006302b8
WeiShengPan <pws522@qq.com>
2015-07-14 14:35:39 +0800

    news module

--- src/main/java/cn/nanyin/dao/NewsDao.java
+++ src/main/java/cn/nanyin/dao/NewsDao.java
@@ -2,17 +2,21 @@ package cn.nanyin.dao;
 
 import cn.nanyin.model.News;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
- * Created by звкЙ on 2015/7/13.
+ * Created by пїЅпїЅпїЅпїЅ on 2015/7/13.
  */
 @Repository
+@Transactional
 public class NewsDao {
+
 
     public List<News> getNewsList(Integer start,Integer max)
     {
+
         return null;
     }
 }
