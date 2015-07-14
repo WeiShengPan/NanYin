d16d760d7dc1fb3232807e819b30e054be7c221a
WeiShengPan <pws522@qq.com>
2015-07-14 16:54:28 +0800

    news module

--- src/main/java/cn/nanyin/dao/NewsDao.java
+++ src/main/java/cn/nanyin/dao/NewsDao.java
@@ -1,6 +1,10 @@
 package cn.nanyin.dao;
 
 import cn.nanyin.model.News;
+import cn.nanyin.model.NewsSort;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +16,22 @@ import java.util.List;
 @Repository
 @Transactional
 public class NewsDao {
-
+    @Autowired
+    private SessionFactory sessionFactory;
 
     public List<News> getNewsList(Integer start,Integer max)
     {
+        Query query=sessionFactory.getCurrentSession().createQuery("from news");
+        query.setFirstResult(start);
+        query.setMaxResults(max);
+        return query.list();
+    }
 
-        return null;
+    public List<NewsSort> getNewsSortList(Integer start,Integer max)
+    {
+        Query query=sessionFactory.getCurrentSession().createQuery("from newssort");
+        query.setFirstResult(start);
+        query.setMaxResults(max);
+        return query.list();
     }
 }
