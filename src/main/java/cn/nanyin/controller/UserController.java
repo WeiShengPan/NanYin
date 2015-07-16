5f450f545b604f538dc8424caeae533d1f07c915
MYNP <1464432469@qq.com>
2015-07-16 19:09:16 +0800

    test

--- src/main/java/cn/nanyin/controller/UserController.java
+++ src/main/java/cn/nanyin/controller/UserController.java
@@ -1,39 +1,67 @@
 package cn.nanyin.controller;
 
+import cn.nanyin.dao.AreaDao;
 import cn.nanyin.dao.UserDao;
+import cn.nanyin.model.Area;
+import cn.nanyin.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by gg on 2015/7/14.
  */
 @Controller
 public class UserController {
+    @Autowired
     private UserDao userDao;
-
-    public UserDao getUserDao() {
-        return userDao;
-    }
-
-    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    @Autowired
+    private AreaDao areaDao;
 
     @RequestMapping(value="/nyadmin/userlist",method= RequestMethod.GET)
-    public ModelAndView showNewsList()
+    public ModelAndView showUserList()
     {
-        return new ModelAndView("nyadmin/userlist");
+        ModelAndView model=new ModelAndView("nyadmin/userlist");
+        List<User> userList =userDao.getUserList(0,50);
+        model.addObject("userList", userList);
+        return model;
     }
-//    @RequestMapping(value="/nyadmin/userlist",method= RequestMethod.GET)
-//    public ModelAndView showUserList()
-//    {
-//        Map map=new HashMap();
-//        map.put("userlist",userDao.getUserList());
-//        return new ModelAndView("nyadmin/userlist",map);
-//    }
+
+    @RequestMapping(value="nyadmin/useraddpage",method=RequestMethod.GET)
+    public ModelAndView showUserAddPage()
+    {
+        ModelAndView model=new ModelAndView("nyadmin/useradd");
+        List<Area> areaList=areaDao.getAreaList(0, 50);
+        model.addObject("areaList",areaList);
+        return model;
+    }
+
+    @RequestMapping(value="nyadmin/useradd",method = RequestMethod.POST)
+    public ModelAndView addUser(User user)
+    {
+        user.setRegDate(new Date());
+        userDao.addUser(user);
+        return new ModelAndView("redirect:useraddpage");
+    }
+
+    @RequestMapping(value="nyadmin/userarea",method=RequestMethod.GET)
+    public ModelAndView showAreaList()
+    {
+        ModelAndView model=new ModelAndView("nyadmin/userarea");
+        List<Area> areaList=areaDao.getAreaList(0, 50);
+        model.addObject("areaList", areaList);
+        return model;
+    }
+
+    @RequestMapping(value="nyadmin/userareaadd",method = RequestMethod.POST)
+    public ModelAndView addArea(Area area)
+    {
+        areaDao.addArea(area);
+        return new ModelAndView("redirect:userarea");
+    }
 }
