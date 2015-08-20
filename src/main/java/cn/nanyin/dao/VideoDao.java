b52457207a33ac0e14e5c2d0367d03f3c0da49c0
WeiShengPan <pws522@qq.com>
2015-08-20 15:55:18 +0800

    video module

--- src/main/java/cn/nanyin/dao/VideoDao.java
+++ src/main/java/cn/nanyin/dao/VideoDao.java
@@ -1,12 +1,14 @@
 package cn.nanyin.dao;
 
 import cn.nanyin.model.Video;
+import cn.nanyin.model.VideoSort;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -62,7 +64,40 @@ public class VideoDao {
         sessionFactory.getCurrentSession().saveOrUpdate(video);
     }
 
+    public List<VideoSort> getVideoSortList(Integer start,Integer max)
+    {
+        Query query=sessionFactory.getCurrentSession().createQuery("from VideoSort");
+        query.setFirstResult(start);
+        query.setMaxResults(max);
+        return query.list();
+    }
 
+    public VideoSort getVideoSortById(long id)
+    {
+        return (VideoSort)sessionFactory.getCurrentSession().get(VideoSort.class,id);
+    }
 
+    public Serializable addVideo(Video video) {
+        Serializable result =  sessionFactory.getCurrentSession().save(video);
+        return result;
+    }
+
+    public Serializable addVideoSort(VideoSort videoSort) {
+        Serializable result = sessionFactory.getCurrentSession().save(videoSort);
+        return result;
+    }
+
+    public void deleteVideo(Video video) {
+        sessionFactory.getCurrentSession().delete(video);
+    }
+
+    public void deleteVideoSort(VideoSort videoSort) {
+        sessionFactory.getCurrentSession().delete(videoSort);
+    }
+
+    public void updateVideoSort(VideoSort videoSort)
+    {
+        sessionFactory.getCurrentSession().saveOrUpdate(videoSort);
+    }
 
 }
